6ebfda85c6f27f0886706ed1d23198e4d87af752
Asa <87864717+tmys949@users.noreply.github.com>
2023-04-17 23:12:18 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,8 +2,8 @@ const BLOG = {
   title: '乌云Blog',
   author: 'Kado',
   email: 'i@craigary.net',
-  link: 'https://blog.kadoi.top',
-  description: '乌云Blog记录点滴.',
+  link: 'https://nobelium.vercel.app',
+  description: 'This gonna be an awesome website.',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
@@ -11,7 +11,7 @@ const BLOG = {
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: , // If leave this empty, current year will be used.
+  since: 2021, // If leave this empty, current year will be used.
   postsPerPage: 7,
   sortByDate: false,
   showAbout: true,
